59e8f59d6d9ae20c1c3a6fe18d390b04586df856
Vladislav Grozin <vlad.grozin@yandex.ru>
2020-12-04 14:39:26 +0300

    Two-stage model added

--- synthetic-kun/interface.py
+++ synthetic-kun/interface.py
@@ -1,8 +1,10 @@
 from scipy.sparse import csc_matrix, dok_matrix
 from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
 from sparsesvd import sparsesvd
 import numpy as np
 from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
 
 
 class SyntheticDataGenerator:
@@ -19,8 +21,11 @@ class SyntheticDataGenerator:
 class MFDataGenerator(SyntheticDataGenerator):
     def __init__(self, ratings, verbose=True):
         super().__init__(ratings)
+
         self.n_users = self.ratings.shape[0]
         self.n_items = self.ratings.shape[1]
+        self.item_ids = list(range(self.n_items))
+
         self.user_vectors = None
         self.item_vectors = None
         self.gmm = None
@@ -44,7 +49,7 @@ class MFDataGenerator(SyntheticDataGenerator):
         item_proba_intercepts = []
 
         for i in range(consider_items):
-            if self.verbose and (i % 100 == 0):
+            if self.verbose and (i % 1000 == 0):
                 print(i)
             try:
                 item_rating_indices = self.ratings.indices[self.ratings.indptr[i]: self.ratings.indptr[i + 1]]
@@ -65,42 +70,55 @@ class MFDataGenerator(SyntheticDataGenerator):
         self.item_proba_vectors = np.array(item_proba_vectors)[:, 0, :]
         self.item_proba_intercepts = np.array(item_proba_intercepts)[:, 0]
 
-    def _sample_users(self, n_users):
-        avg_ratings_per_user = len(self.ratings.nonzero()[0]) / self.n_users
-
-        user_vectors, _ = self.gmm.sample(n_users)
-        ratings_per_user = np.random.exponential(avg_ratings_per_user, size=n_users)
-
-        return user_vectors, ratings_per_user
-
     def build(self, components=200, gmm_clusters=10, consider_items=3000):
         self.consider_items = consider_items
 
         self._build_mf(components)
-        self._build_gmm(gmm_clusters)
         self._build_implicit_mf(consider_items)
+        self._build_gmm(gmm_clusters)
 
-    def generate(self, n_users):
+    def _sample_users(self, n_users):
+        avg_ratings_per_user = len(self.ratings.indices) / self.n_users
+
+        proper_users = np.where(np.array(self.ratings.sum(axis=1) > 50))
+
+        user_vectors, _ = self.gmm.sample(n_users)
+        user_vectors = np.array(np.random.choice(proper_users, size=n_users, replace=True))
+        ratings_per_user = np.random.exponential(avg_ratings_per_user, size=n_users)
+
+        return user_vectors, ratings_per_user
+
+    def _sample_user_items(self, user_vector, n_items):
+        dotproducts = np.dot(self.item_proba_vectors, user_vector) + self.item_proba_intercepts
+        logits = 1.0 / (1e-7 + np.exp(-dotproducts))
+        logits = logits / np.sum(logits)
+
+        sampled_items = np.random.choice(self.item_ids, n_items, False, p=logits)
+
+        return sampled_items
+
+    def _get_user_rating(self, user_vector, item):
+        sampled_rating = user_vector.dot(self.item_vectors[item])
+        return sampled_rating
+
+    def generate(self, n_users, use_actual_user_vectors=False):
         user_vectors, ratings_per_user = self._sample_users(n_users)
 
         rating_matrix = dok_matrix((n_users, self.consider_items))
 
-        ids = list(range(self.consider_items))
-
         for u in range(n_users):
-            ratings_count = int(np.random.exponential(ratings_per_user[u]))
+            ratings_count = int(ratings_per_user[u])
             ratings_count = min(ratings_count, self.consider_items)
 
-            v = user_vectors[u, :]
+            if use_actual_user_vectors:
+                v = user_vectors[u, :]
+            else:
+                v = user_vectors[u, :]
 
-            logits = np.exp(np.dot(self.item_proba_vectors, v) + self.item_proba_intercepts)
-            logits = logits / np.sum(logits)
+            sampled_items = self._sample_user_items(v, ratings_count)
 
-            sampled_items = np.random.choice(ids, ratings_count, False, p=logits)
-            sampled_rating = self.item_vectors[sampled_items].dot(v)
-            sampled_rating = np.minimum(1.0, np.maximum(sampled_rating, -1.0))
-
-            for i, r in zip(sampled_items, sampled_rating):
-                rating_matrix[u, i] = r
+            for i in sampled_items:
+                rating_matrix[u, i] = self._get_user_rating(v, i)
+                rating_matrix[u, i] = np.minimum(1.0, np.maximum(rating_matrix[u, i], -1.0))
 
         return rating_matrix
